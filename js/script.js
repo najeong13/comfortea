d4c190f3dc18bada21270de2efa63ed159096c7f
najeong13 <najeong.u.u@gmail.com>
2024-12-11 14:57:14 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// section3 prd-text
+// $("#panel").hide();
+// $("#inbtn").mouseenter(function () { $("#panel").slideDown() });
+// $("#panel").hide();
+// $("#outbtn").mouseleave(function () { $("#panel").slideUp() });
+// $("#panel").hide();
+// $("#inout").mouseenter(function () { $("#panel").slideDown() });
+// $("#panel").hide();
+// $("#inout").mouseleave(function () { $("#panel").slideUp() });
+
